db059f759ca399056299e7bf71eb7304f0235487
afifasoft <abdul.pesit@gmail.com>
2021-08-31 22:36:49 +0530

    first commit

---
+++ src/main/java/com/cloudchainsoftware/cloudpetclinic/CloudPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.cloudchainsoftware.cloudpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudchainsoftware/cloudpetclinic/CloudPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudchainsoftware.cloudpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
